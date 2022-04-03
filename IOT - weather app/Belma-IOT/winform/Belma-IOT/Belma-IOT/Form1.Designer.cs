cee35f163a8ea3f716f2f7c50df224bdab778c75
Belma Nukić <nukicbelma@hotmail.com>
2022-04-03 19:03:21 +0200

    weatherapp - temp i vlaznost zraka

---
+++ IOT - weather app/Belma-IOT/winform/Belma-IOT/Belma-IOT/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace Belma_IOT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTemp = new System.Windows.Forms.Label();
+            this.lblVlaznost = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Navy;
+            this.label1.Location = new System.Drawing.Point(17, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Trenutna temperatura iznosi: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Navy;
+            this.label2.Location = new System.Drawing.Point(17, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(188, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Trenutna vlaznost zraka iznosi: ";
+            // 
+            // lblTemp
+            // 
+            this.lblTemp.AutoSize = true;
+            this.lblTemp.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTemp.ForeColor = System.Drawing.Color.Navy;
+            this.lblTemp.Location = new System.Drawing.Point(210, 37);
+            this.lblTemp.Name = "lblTemp";
+            this.lblTemp.Size = new System.Drawing.Size(34, 13);
+            this.lblTemp.TabIndex = 2;
+            this.lblTemp.Text = "temp";
+            // 
+            // lblVlaznost
+            // 
+            this.lblVlaznost.AutoSize = true;
+            this.lblVlaznost.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVlaznost.ForeColor = System.Drawing.Color.Navy;
+            this.lblVlaznost.Location = new System.Drawing.Point(211, 155);
+            this.lblVlaznost.Name = "lblVlaznost";
+            this.lblVlaznost.Size = new System.Drawing.Size(40, 13);
+            this.lblVlaznost.TabIndex = 3;
+            this.lblVlaznost.Text = "vlznst";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(284, 199);
+            this.Controls.Add(this.lblVlaznost);
+            this.Controls.Add(this.lblTemp);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WeatherApp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblTemp;
+        private System.Windows.Forms.Label lblVlaznost;
+    }
+}
+
---
+++ IOT - weather app/Belma-IOT/winform/Belma-IOT/Belma-IOT/Form1.cs
@@ -0,0 +1,99 @@
+﻿using FireSharp.Config;
+using FireSharp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Belma_IOT
+{
+    public partial class Form1 : Form
+    {
+        public class dataFbase
+        {
+            public string Temperatura { get; set; }
+            public String Vlaznost { get; set; }
+        }
+
+        public SerialPort myport;
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            myport = new SerialPort();
+
+            myport.PortName = "COM3";
+            myport.BaudRate = 9600;
+            myport.DtrEnable = true;
+            myport.Open();
+
+            myport.DataReceived += serialPort1_DataReceived;
+            client = new FireSharp.FirebaseClient(config);
+        }
+
+        IFirebaseConfig config = new FirebaseConfig()
+        {
+            AuthSecret = "8hNPFHKfLluXQnvpaVkgFFZSMc4PuywZfumtlpOw",
+            BasePath = "https://iot-belma-default-rtdb.europe-west1.firebasedatabase.app/"
+        };
+
+        IFirebaseClient client;
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            string line = myport.ReadLine();
+            this.BeginInvoke(new LineReceivedEvent(LineReceived), line);
+        }
+
+        private delegate void LineReceivedEvent(string line);
+        private void LineReceived(string portporuka)
+        {
+
+            var fdata = new dataFbase();
+
+            if (portporuka.Contains("temperature"))
+            {
+                int vrijednosttemperature = Int32.Parse(portporuka.Substring("temperature = ".Length, 2));
+
+                lblTemp.Text = vrijednosttemperature.ToString() +"C";
+                if(vrijednosttemperature>30) {
+                    lblTemp.ForeColor = Color.Red;
+                }
+                else 
+                    lblTemp.ForeColor = Color.Green;
+            }
+            if (portporuka.Contains("humidity"))
+            {
+                int vrijednostvlaznosti = Int32.Parse(portporuka.Substring("Current humidity = ".Length, 2));
+                lblVlaznost.Text = vrijednostvlaznosti.ToString() + "%";
+            }
+
+            fdata.Temperatura = lblTemp.Text;
+            fdata.Vlaznost = lblVlaznost.Text;
+            //client.Push("/", fdata);
+            client.UpdateAsync("", fdata);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
